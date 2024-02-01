99394f6e92446f02066b1f005dfce4da022e99e4
JavlonUMM <javlonummofficial@gmail.com>
2024-02-01 12:22:25 +0500

    gunicorn ornatildi

--- product/config/settings.py
+++ product/config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-5jakj_$=^amltx_qpi!^k@xg=-lr+mr8=f-_qo91(p#(75s=(w
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -122,6 +122,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',]
+
+STATIC_ROOT = BASE_DIR / 'sta'
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
